7dfdf8c5d499a8d9d7af1e13d8b27705a8c198fa
Léane <drsleane@gmail.com>
2022-06-28 17:02:11 +0200

    testing code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("The line will be printed")
+
+myint = 7
+print(myint)
+
+mystring = "Hello everyone"
+print(mystring)
+
+num1 = 1
+num2 = 9
+num3 = num1 + num2
+print(num3)
+
+test1 = "coucou"
+test2 = "les loulous"
+print(test1 + " " + test2)
